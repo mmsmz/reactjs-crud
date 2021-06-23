11ae7e9a11f660dfd704d7d3e303dcae0dff6b7b
mohamed <marafath07@gmail.com>
2021-06-24 03:07:07 +0530

    initailized the crud app

---
+++ src/components/counter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() { 
+
+        return (<div><h1>HELLO WORLD</h1><button class="btn btn-primary">Increment</button></div>);
+    }
+}
+ 
+export default Counter;
