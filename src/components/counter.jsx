9556b6d9811a0eba63eb99d699fc93c4472f5ac5
mohamed <marafath07@gmail.com>
2021-06-25 23:48:21 +0530

    updated the data

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    render() { 
+    state = {
+        count: 0
+        // imageUrl: 'https://picsum.photos/200'
+        // address : {
+        //     street: ''
+        // }
+    };
+    // style = {
+    //     fontSize: 40,
+    //     fontWeight: "bold"
 
-        return (<div><h1>HELLO WORLD</h1><button class="btn btn-primarycd">Increment</button></div>);
+    // };
+
+    render() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        // return (<React.Fragment><h1>HELLO WORLD</h1><button class="btn btn-primary">Increment</button></React.Fragment>);
+        return (
+            <div>
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button class="btn btn-primary">Increment</button>
+            </div >
+        );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
+        // return this.state.count === 0 ? 'Zero' : this.state.count;
+
     }
 }
- 
+
 export default Counter;
